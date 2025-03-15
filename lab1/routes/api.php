6eb562b8d97b8652eee2d4ed188022afd2b00059
Volodymyr <134626794+volodymyr-tsukanov@users.noreply.github.com>
2025-03-15 16:20:25 +0100

    lab1.p2.e2

--- lab1/routes/api.php
+++ lab1/routes/api.php
@@ -4,14 +4,30 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 //use App\Models\Product;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
 
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::resource('products', ProductController::class);
+
+//Trasy publiczne
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+//Trasy chronione
+Route::group(['middleware' => ['auth:sanctum']], function() {
+Route::post('/products', [ProductController::class, 'store']);
+Route::patch('/products/{id}', [ProductController::class, 'update']);
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+//Route::resource('products', ProductController::class);
+//Route::get('/products/search/{name}', [ProductController::class, 'search']);
 /*Route::get('/products', function(){
     return Product::all();
 });*/
