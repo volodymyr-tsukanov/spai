100ab97b5765a75891af6c1592692745bd35874c
Volodymyr <134626794+volodymyr-tsukanov@users.noreply.github.com>
2025-03-19 16:46:11 +0100

    finish lab1 (5/5) ; lab1.p2.f1(fixed sanctum login/register)

--- lab1/app/Models/User.php
+++ lab1/app/Models/User.php
@@ -6,11 +6,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -22,6 +24,9 @@ class User extends Authenticatable
         'email',
         'password',
     ];
+
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that should be hidden for serialization.
