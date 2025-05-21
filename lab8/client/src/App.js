d8ff17f771e23ae4f07faee177bfb2f28ab8f61a
Volodymyr <134626794+volodymyr-tsukanov@users.noreply.github.com>
2025-05-21 16:31:48 +0200

    lab8.1 (fixed React 18.+)

--- lab8/client/src/App.js
+++ lab8/client/src/App.js
@@ -1,7 +1,7 @@
 import { Route, Routes, Navigate } from "react-router-dom"
-import Main from "./components/Main"
-import Signup from "./components/Signup"
-import Login from "./components/Login"
+import Main from "./Main"
+import Signup from "./Signup"
+import Login from "./Login"
 
 
 function App() {
