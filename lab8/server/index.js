eb9aae0eaf2639f7e0a26c0752b461d8615ebc78
Volodymyr <134626794+volodymyr-tsukanov@users.noreply.github.com>
2025-05-21 16:20:11 +0200

    lab8.1

--- lab8/server/index.js
+++ lab8/server/index.js
@@ -3,12 +3,17 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const connection = require('./db')
+const userRoutes = require("./routes/users")
+const authRoutes = require("./routes/auth")
 
-
+// mongodb
 connection()
 
 //middleware
 app.use(express.json())
 app.use(cors())
+app.use("/api/users", userRoutes)
+app.use("/api/auth", authRoutes)
+
 const port = process.env.PORT || 8080
 app.listen(port, () => console.log(`Nasłuchiwanie na porcie ${port}`))
